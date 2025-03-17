ba7d39bde82becb76ae3454f5665120322426892
Yura <yurchyk2007@gmail.com>
2025-03-17 18:57:47 +0100

    Add project files.

---
+++ password_window_windows_forms/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace password_window_windows_forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtPassword = new TextBox();
+            lblCharCount = new Label();
+            SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(253, 32);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(242, 27);
+            txtPassword.TabIndex = 0;
+            // 
+            // lblCharCount
+            // 
+            lblCharCount.AutoSize = true;
+            lblCharCount.Location = new Point(578, 41);
+            lblCharCount.Name = "lblCharCount";
+            lblCharCount.Size = new Size(50, 20);
+            lblCharCount.TabIndex = 1;
+            lblCharCount.Text = "label1";
+            lblCharCount.Click += lblCharCount_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblCharCount);
+            Controls.Add(txtPassword);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtPassword;
+        private Label lblCharCount;
+    }
+}
---
+++ password_window_windows_forms/Form1.cs
@@ -0,0 +1,58 @@
+﻿namespace password_window_windows_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtPassword.PasswordChar = '*';  
+            UpdateCharacterCount();  
+
+          
+            txtPassword.TextChanged += txtPassword_TextChanged;
+            txtPassword.KeyPress += txtPassword_KeyPress;
+            txtPassword.KeyDown += txtPassword_KeyDown;
+        }
+
+
+      
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == ' ')
+            {
+                e.Handled = true; 
+            }
+        }
+
+     
+        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Back)
+            {
+                UpdateCharacterCount();  
+            }
+        }
+
+        
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            UpdateCharacterCount();  
+        }
+
+      
+        private void UpdateCharacterCount()
+        {
+          
+            if (lblCharCount != null)
+            {
+                lblCharCount.Text = $"Символів: {txtPassword.Text.Length}";
+            }
+        }
+
+        
+        private void lblCharCount_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
